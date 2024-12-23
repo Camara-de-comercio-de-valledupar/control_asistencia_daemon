8cffd1350bfd9aa4e60aa966fcfe622b672f875e
Andreeccv <aprendizauditoria2@ccvalledupar.org.co>
2024-12-23 16:20:12 -0500

    Agregar archivo para vista de reportes entradas tarde
    Agregue un archivo llamado
    lib/screens/late_assistances_report.screen.dart
    
    Que tendra los widgets para la vista de reporte de entradas tarde.

---
+++ lib/screens/late_assistances_report.screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LateAssistancesReportScreen extends StatelessWidget {
+  const LateAssistancesReportScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,4 +1,5 @@
 export 'dashboard.screen.dart';
+export 'late_assistances_report.screen.dart';
 export 'loading.screen.dart';
 export 'login.screen.dart';
 export 'not_found_screen.dart';
