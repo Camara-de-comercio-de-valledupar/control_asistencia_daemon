6d34a3385a5410e5881e5c5501e8149a6b66a988
desarrolladorccv <desarrollador@ccvalledupar.org.co>
2025-01-02 10:19:11 -0500

    Agregar documentación y métodos para la autenticación y obtención de permisos en AuthenticationService

--- lib/services/authentication.service.dart
+++ lib/services/authentication.service.dart
@@ -1,13 +1,21 @@
-import 'dart:async';
-
 import 'package:control_asistencia_daemon/lib.dart';
 
+/// Servicio de Autenticación
+///
+/// Proporciona métodos para la autenticación de usuarios y la obtención de permisos.
 class AuthenticationService {
+  /// Cliente HTTP utilizado para realizar solicitudes.
   final HttpClient _client;
+
+  /// Instancia única de AuthenticationService.
   static AuthenticationService? _instance;
 
+  /// Constructor privado para inicializar el servicio de autenticación con un cliente HTTP.
   AuthenticationService(this._client);
 
+  /// Obtiene la instancia única de AuthenticationService.
+  ///
+  /// Si la instancia no existe, se crea una nueva utilizando un cliente HTTP.
   static AuthenticationService getInstance() {
     _instance ??= AuthenticationService(
       HttpClient.getInstance(),
@@ -15,6 +23,17 @@ class AuthenticationService {
     return _instance!;
   }
 
+  /// Inicia sesión con correo electrónico y contraseña.
+  ///
+  /// Realiza una solicitud POST a la ruta "/autenticar" con el correo electrónico y la contraseña proporcionados.
+  /// Luego, obtiene información adicional del miembro autenticado desde la ruta "/hojasdevidas/{id}".
+  /// Si el miembro tiene una foto, se agrega la URL completa de la foto a los datos del miembro.
+  ///
+  /// Retorna una instancia de [Member] con los datos del miembro autenticado.
+  ///
+  /// Parámetros:
+  /// - [email]: Correo electrónico del usuario.
+  /// - [password]: Contraseña del usuario.
   Future<Member> signInWithEmailAndPassword(
       String email, String password) async {
     final response = await _client.post(
@@ -44,6 +63,14 @@ class AuthenticationService {
     return Member.fromJson(data);
   }
 
+  /// Obtiene los permisos de un miembro.
+  ///
+  /// Realiza una solicitud GET a la ruta "/GetMenuFuncionario/empleados_id/{memberId}" para obtener los permisos del miembro con el ID proporcionado.
+  ///
+  /// Retorna una lista de instancias de [Permission] con los permisos del miembro.
+  ///
+  /// Parámetros:
+  /// - [memberId]: ID del miembro.
   Future<List<Permission>> getPermissions(int memberId) async {
     final response =
         await _client.get("/GetMenuFuncionario/empleados_id/$memberId");
